79576e6b73fc48d3ecd8f286ac1430a7937fd91d
Paul Smith <paul.j.smith@oracle.com>
2021-01-11 19:36:40 +1100

    Rework Axios Promise usage to be a bit better (still learning...)

--- solaredgeapi/index.js
+++ solaredgeapi/index.js
@@ -20,15 +20,15 @@ module.exports = class SolarEdgeAPI {
         };
         return this.axios.get(`/currentPowerFlow.json?api_key=${this.apiKey}`)
             .then((response) => {
-                response.data.siteCurrentPowerFlow.connections.forEach((item) => {
-                    if (item.from == 'LOAD' && item.to == 'Grid') {
-                        powerFlow.state = 'exporting'
-                    }
-                });
-                powerFlow.power = response.data.siteCurrentPowerFlow.GRID.currentPower;
-                //console.log(powerFlow);
-                return powerFlow;
-            }
+                    response.data.siteCurrentPowerFlow.connections.forEach((item) => {
+                        if (item.from == 'LOAD' && item.to == 'Grid') {
+                            powerFlow.state = 'exporting'
+                        }
+                    });
+                    powerFlow.power = response.data.siteCurrentPowerFlow.GRID.currentPower;
+                    //console.log(powerFlow);
+                    return powerFlow;
+                }
             ).catch((error) => console.log(error));
     }
 
@@ -44,29 +44,27 @@ module.exports = class SolarEdgeAPI {
             }
         }
 
-        this.axios.get(`/dataPeriod.json?api_key=${this.apiKey}`)
+        return this.axios.get(`/dataPeriod.json?api_key=${this.apiKey}`)
             .then((response) => {
                 var period = response.data.dataPeriod;
                 this.startTime = period.startDate + ' 00:00:00';
                 this.endTime = period.endDate + ' 23:59:59';
                 energyInfo.duration = moment.duration(moment(this.endTime).diff(moment(this.startTime))).asDays();
             })
-            .catch((error) => console.log(error));
-
-        return this.axios
-            .get(`/energyDetails.json?api_key=${this.apiKey}&timeUnit=YEAR&startTime=${this.startTime}&endTime=${this.endTime}`)
-            .then((response) => {
-                var meters = response.data.energyDetails.meters;
-                meters.forEach(function (item) {
-                    var total = 0;
-                    item.values.forEach((item, index) => total += item.value / 1000); // TODO need to be sure of the Units are still "Wh"
-                    energyInfo.energy[item.type] = total;
-                });
-                //console.log(energyInfo);
-                return energyInfo;
+            .then(() => {
+                return this.axios
+                    .get(`/energyDetails.json?api_key=${this.apiKey}&timeUnit=YEAR&startTime=${this.startTime}&endTime=${this.endTime}`)
+                    .then((response) => {
+                        var meters = response.data.energyDetails.meters;
+                        meters.forEach(function (item) {
+                            var total = 0;
+                            item.values.forEach((item, index) => total += item.value / 1000); // TODO need to be sure of the Units are still "Wh"
+                            energyInfo.energy[item.type] = total;
+                        });
+                        //console.log(energyInfo);
+                        return energyInfo;
+                    })
             })
             .catch((error) => console.log(error));
     }
-
-
 };
