ed2027e39ae28241dbd80439d2739cfa011fadda
Paul Smith <paul.j.smith@oracle.com>
2021-01-11 19:35:18 +1100

    Use dotenv to source the SiteID and API_KEY - note, I have revoked any previous API key used in the source code.

--- testbed.js
+++ testbed.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const SolarEdgeAPI = require('./solaredgeapi/index.js');
 const SufficientSolarChecker = require('./sufficientsolarchecker');
 const ROICalculator = require('./roicalculator')
@@ -6,19 +8,24 @@ const costs = {
     energyPrice: 29.5,
     feedInTariff: 10.2
 }
-var api = new SolarEdgeAPI('1978596', 'ZY1Z0BQ5692HFXCPL3CD3HB7P5HU4WEU');
+
+var api = new SolarEdgeAPI(process.env.SITE_ID, process.env.API_KEY);
 let sufficientSolarChecker = new SufficientSolarChecker();
 
 api.currentPowerFlow().then((flow) => {
     console.log('Powerflow:' + JSON.stringify(flow, null, 2));
-    api.getEnergyInfo().then((resolved,rejected) => {
+    return flow;
+}).then((flow)=>{
+    api.getEnergyInfo().then((resolved) => {
         let energyInfo = resolved;
         var roiCalc = new ROICalculator(costs, energyInfo.duration);
         console.log('ROI:' + JSON.stringify(roiCalc.calculate(energyInfo.energy), null, 2));
         console.log('Ok to use High Powered Device?: ' + sufficientSolarChecker.isOkToUseHighPoweredDevices(energyInfo, flow));
     });
-
+}).catch((error)=>{
+    console.log(error);
 });
+
 
 
 
