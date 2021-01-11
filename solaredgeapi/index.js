f1df265ffe53a48b753c9eb0adec29c34e786d5f
Paul Smith <paul.j.smith@oracle.com>
2021-01-11 12:24:18 +1100

    Refactoring and adding currentPowerFlow details

--- solaredgeapi/index.js
+++ solaredgeapi/index.js
@@ -1,4 +1,5 @@
 const AXIOS = require('axios');
+const moment = require('moment');
 
 module.exports = class SolarEdgeAPI {
     constructor(siteId, apiKey) {
@@ -6,38 +7,66 @@ module.exports = class SolarEdgeAPI {
         this.apiKey = apiKey;
         this.startTime = '';
         this.endTime = '';
+        this.durationInDays = 0;
         this.axios = AXIOS.create({
             baseURL: `https://monitoringapi.solaredge.com/site/${this.siteId}`
         });
+    }
+
+    currentPowerFlow() {
+        const powerFlow = {
+            state: 'importing',
+            power: 0
+        };
+        return this.axios.get(`/currentPowerFlow.json?api_key=${this.apiKey}`)
+            .then((response) => {
+                response.data.siteCurrentPowerFlow.connections.forEach((item) => {
+                    if (item.from == 'LOAD' && item.to == 'Grid') {
+                        powerFlow.state = 'exporting'
+                    }
+                });
+                powerFlow.power = response.data.siteCurrentPowerFlow.GRID.currentPower;
+                //console.log(powerFlow);
+                return powerFlow;
+            }
+            ).catch((error) => console.log(error));
+    }
+
+    getEnergyInfo() {
+        var energyInfo = {
+            duration: 0,
+            energy: {
+                Production: 0,
+                SelfConsumption: 0,
+                FeedIn: 0,
+                Purchased: 0,
+                Consumption: 0
+            }
+        }
+
         this.axios.get(`/dataPeriod.json?api_key=${this.apiKey}`)
             .then((response) => {
                 var period = response.data.dataPeriod;
                 this.startTime = period.startDate + ' 00:00:00';
                 this.endTime = period.endDate + ' 23:59:59';
-            });
+                energyInfo.duration = moment.duration(moment(this.endTime).diff(moment(this.startTime))).asDays();
+            })
+            .catch((error) => console.log(error));
 
-    }
-
-    getROIData(callback) {
-        var theEnergy = {
-            Production: 0,
-            SelfConsumption: 0,
-            FeedIn: 0,
-            Purchased: 0,
-            Consumption: 0
-        };
         return this.axios
             .get(`/energyDetails.json?api_key=${this.apiKey}&timeUnit=YEAR&startTime=${this.startTime}&endTime=${this.endTime}`)
             .then((response) => {
                 var meters = response.data.energyDetails.meters;
-                meters.forEach(function (item, index) {
+                meters.forEach(function (item) {
                     var total = 0;
                     item.values.forEach((item, index) => total += item.value / 1000); // TODO need to be sure of the Units are still "Wh"
-                    theEnergy[item.type] = total;
+                    energyInfo.energy[item.type] = total;
                 });
-                return theEnergy;
+                //console.log(energyInfo);
+                return energyInfo;
             })
-            .then(callback)
             .catch((error) => console.log(error));
     }
+
+
 };
--- testbed.js
+++ testbed.js
@@ -1,10 +1,17 @@
 const SolarEdgeAPI = require('./solaredgeapi/index.js');
+const ROICalculator = require('./roicalculator')
 
 const costs = {
     energyPrice: 29.5,
     feedInTariff: 10.2
 }
-var api = new SolarEdgeAPI('1978596', 'ZY1Z0BQ5692HFXCPL3CD3HB7P5HU4WEU', costs);
-console.log(
-    api.getROIData()
-);
+var api = new SolarEdgeAPI('1978596', 'ZY1Z0BQ5692HFXCPL3CD3HB7P5HU4WEU');
+
+api.currentPowerFlow().then((flow) => console.log(flow));
+
+api.getEnergyInfo().then((resolved,rejected) => {
+    let energyInfo = resolved;
+    //console.log(energyInfo);
+    var roiCalc = new ROICalculator(costs, energyInfo.duration);
+    console.log(roiCalc.calculate(energyInfo.energy));
+})
